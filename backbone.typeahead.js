527c38f8a01e8b174f8a0e5458fa569223eb4e0a
aodin <aaronoellis@gmail.com>
2013-07-24 09:06:56 -0700

    Typeahead now extends from Backbone.View instead of Collection

--- backbone.typeahead.js
+++ backbone.typeahead.js
@@ -1,307 +1,41 @@
 (function() {
   var Typeahead = function(models, options) {
-    // Immediately create a copy of the given models
-    // TODO Allow another collection to be used as models
-    models = _.isArray(models) ? models.slice(0) : [];
-    this.preInitialize.apply(this, arguments);
-    Backbone.Collection.apply(this, arguments);
-    this.postInitialize.apply(this, arguments);
+    // The first parameter 'model' is an optional parameter
+    // If given an array, a copy will be made and passed to preInitialize
+    if (_.isArray(models)) {
+      // It is copied to prevent overwrite
+      // TODO this copy may be unnecessary
+      models = models.slice(0);
+      if (!_.isObject(options)) options = {};
+    } else if (_.isObject(models)) {
+      options = models;
+      // TODO Should also error if no collection was provided
+    } else {
+      throw new Error('A typeahead must be created with either initial models or have its collection specified in an options object');
+    }
+
+    // Initialize
+    this.preInitialize.call(this, models, options);
+    Backbone.View.call(this, options);
+    this.postInitialize.call(this);
   }
 
-  Typeahead.extend = Backbone.Collection.extend;
+  Typeahead.extend = Backbone.View.extend;
 
-  Typeahead.Model = Backbone.Model.extend({
-    getView: function() {
-      if (_.isUndefined(this.view)) {
-        this.view = new Typeahead.Item({model: this});
-      }
-      return this.view;
-    },
-  });
-
-  _.extend(Typeahead.prototype, Backbone.Collection.prototype, {
-    model: Typeahead.Model,
+  _.extend(Typeahead.prototype, Backbone.View.prototype, {
     // Pre-initialization should set up the collection type (remote / local)
-    // and clear out the models that would be passed to the initialize
     preInitialize: function(models, options) {
-      options || (options = {});
-      // Attaching url to "this" also occurs in the constructor
-      if (options.url) this.url = options.url;
-      if (_.result(this, 'url')) {
-        _.extend(this, Typeahead.RemoteCollection);
-      } else if (models && models.length) {
-        _.extend(this, Typeahead.LocalCollection);
-        this._queryset = models.slice(0);
-        // Clear out the referenced array object
-        models.length = 0;
-      } else {
-        // For now, the typeahead must have either a URL or initial models
-        throw new Error('A typeahead must be created with either initial models (creating a local typeahead) or with a url in the options (creating a remote typeahead)');
-      }
-    },
-    // Models were emptied by the preInitialization function. The parent
-    // Backbone.Collection constructor function should have finished.
-    postInitialize: function(models, options) {
-      // Parse the given options, providing sane defaults where necessary
-      options || (options = {});
-      this.options = options;
+      // Set sane defaults
+      options.key || (options.key = 'name');
 
-      // If no search key is given, default to 'name'
-      this._key = options.key || 'name';
-
-      // TODO what should be loaded directly into the namespace and what should
-      // be left in options?
-
-      // Create the view, it should either be provided an 'el' in options or
-      // have its setElement function called
-      _.extend(options, {collection: this});
-      this.view = new Typeahead.View(options);
-
-      // TODO Defaults using .extend()
-      options.items || (options.items = 8);
-
-      // menu: '<ul class="typeahead dropdown-menu"></ul>'
-      // item: '<li><a href="#"></a></li>'
-      // minLength: 1
+      // TODO Build a collection if none was given
+      // TODO Build a item view if none was provided
     },
-    render: function(selector) {
-      // A proxy to the view's render method
-      if (selector) this.view.setElement(selector);
-      this.view.render();
-    },
-  });
-
-  Typeahead.LocalCollection = {
-    matcher: function(item, value) {
-      return ~item.toLowerCase().indexOf(value.toLowerCase());
-    },
-    search: function(key, value) {
-      // TODO trim value before truthiness testing?
-      // Search should be as fast as possible - this does not seem very fast
-      var items = value ? _.filter(this._queryset, function(item) { return this.matcher(item[key], value); }, this) : [];
-      this.reset(items.slice(0, this.options.items));
-    },
-  };
-
-  Typeahead.RemoteCollection = {
-    // the 'matcher' function is now a server-side operation
-    search: function(key, value) {
-      // FYI this behavior varies from the normal list
-      // On an empty search, it resets the collection
-      this._data = this._data || {};
-      if (!value) {
-        delete this._data[key];
-        this.reset();
-        return;
-      }
-      this._data[key] = value;
-      this.fetch({data: this._data, reset: true});
-    },
-  };
-
-  Typeahead.Item = Backbone.View.extend({
-    tagName: 'li',
-    // TODO by default, this template should display the given key
-    template: '<a><%= _.escape(name) %></a>',
-    events: {
-      'click': 'selectItem',
-    },
-    render: function() {
-      this.$el.html(_.template(this.template, this.model.toJSON()));
-      return this;
-    },
-    selectItem: function() {
-      // TODO needs to be combined with select trigger
-      this.model.trigger('preselect', this.model);
-    },
-  });
-
-  Typeahead.View = Backbone.View.extend({
-    template: '<input type="text" placeholder="Search" /><ul class="typeahead dropdown-menu"></ul>',
-    events: {
-      'keyup': 'keyup',
-      'keypress': 'keypress',
-      'keydown': 'keydown',
-      'blur input': 'blur',
-      'focus input': 'focus',
-      'mouseenter': 'mouseenter',
-      'mouseleave': 'mouseleave',
-    },
-    initialize: function(options) {
-      // Parse options passed from the parent collection
-      if (options.template) this.template = options.template;
-
-      this.listenTo(this.collection, 'preselect', this.selectModel);
-      this.listenTo(this.collection, 'reset', this.renderItems);
-
-      // Boolean toggles
-      this.focused = false;
-      this.shown = false;
-      this.mousedover = false;
-      // Rendering
-      if (options && options.el) this.el = options.el;
-      this.setElement(this.el).render();
-    },
-    render: function() {
-      this.$el.html(_.template(this.template));
-      this.$menu = this.$('ul');
-      // TODO only supports first input for now - more specific selector?
-      this.$input = this.$('input').first();
-      return this;
-    },
-    renderItems: function() {
-      this.$menu.empty();
-      this.shown = Boolean(this.collection.length);
-      _.each(this.collection.models, this.appendItem, this);
-      // CSS is handled within the show() and hide() methods
-      if (this.shown) {
-        this.show();
-      } else {
-        this.hide();
-      }
-    },
-    appendItem: function(model) {
-      this.$menu.append(model.getView().render().el);
-      // TODO separate model or just use default?
-      // var itemView = new Typeahead.Item({model: model});
-      // this.$menu.append(itemView.render().el);
-    },
-    searchItems: function(e) {
-      // TODO wait for input greater than a minimum number of characters
-      // TODO put key second so it can be optional?
-      // Only remove the selected model if the input has changed
-      // TODO last selected model?
-      var val = this.$input.val();
-      if (this._prev && this._prev !== val) {
-        if (!_.isUndefined(this.collection.selected)) {
-          this.collection.selected = undefined;
-          this.collection.trigger('deselect', this);
-        }
-      }
-      this._prev = val;
-      this.collection.search(this.collection._key, val);
-    },
-    keydown: function(e) {
-      this.suppressKeyPressRepeat = ~$.inArray(e.keyCode, [40,38,9,13,27]);
-      this.move(e);
-    },
-    keypress: function(e) {
-      // The suppressKeyPressRepeat check exists because keydown and keypress
-      // may fire for the same event
-      if (this.suppressKeyPressRepeat) return;
-      this.move(e);
-    },
-    keyup: function(e) {
-      switch(e.keyCode) {
-        case 40: // Down arrow
-        case 38: // Up arrow
-        case 16: // Shift
-        case 17: // Ctrl
-        case 18: // Alt
-          break;
-        // case 9: // Tab - disabled to prevent rogue select on tabbed focus
-        // TODO tab should also leave focus
-        case 13: // Enter
-          // TODO shown needs to be returned to its original function (as an
-          // indicator of whether the menu is currently displayed or not)
-          if (!this.shown) return;
-          this.select();
-          break;
-        case 27: // escape
-          if (!this.shown) return;
-          this.hide();
-          break
-        default:
-          this.searchItems(e);
-      }
-      e.stopPropagation();
-      e.preventDefault();
-    },
-    show: function() {
-      // Do not show the menu if no models exist
-      if (!this.collection.length) return;
-      var pos = $.extend(
-        {},
-        this.$input.position(),
-        // FYI the [0] index is needed to return the vanilla HTML object
-        {height: this.$input[0].offsetHeight}
-      );
-      this.$menu.css({
-        top: pos.top + pos.height,
-        left: pos.left
-      }).show();
-      this.shown = true;
-      return this;
-    },
-    hide: function() {
-      this.$menu.hide();
-      this.shown = false;
-      return this;
-    },
-    select: function() {
-      var index = this.$menu.find('.active').index();
-      // TODO this is a brittle method of accessing the underlying models
-      this.selectModel(this.collection.models[index]);
-    },
-    selectModel: function(model) {
-      this.collection.selected = model;
-      this.$input.val(model.get(this.collection._key));
-      this.hide();
-      // Pass the typeahead itself as the second trigger parameter
-      this.collection.trigger('select', model, this);
-      // Reset the collection and set its search data to an empty value
-      // TODO Add the selected model as the only item?
-      this.collection.search(this.collection._key, '');
-    },
-    move: function(e) {
-      if (!this.shown) return;
-      switch(e.keyCode) {
-        case 9: // Tab
-        case 13: // Enter
-        case 27: // Escape
-          e.preventDefault();
-          break;
-        case 38: // Up arrow
-          e.preventDefault();
-          this.prevItem();
-          break;
-        case 40: // Down arrow
-          e.preventDefault();
-          this.nextItem();
-          break;
-      }
-      e.stopPropagation();
-    },
-    prevItem: function() {
-      // TODO should prev and next be signals sent to the collection?
-      var active = this.$menu.find('.active').removeClass('active');
-      var prev = active.prev();
-      if (!prev.length) prev = this.$menu.find('li').last();
-      prev.addClass('active');
-    },
-    nextItem: function() {
-      var active = this.$menu.find('.active').removeClass('active');
-      var next = active.next();
-      if (!next.length) next = $(this.$menu.find('li')[0]);
-      next.addClass('active');
-    },
-    focus: function() {
-      // Only show the results list if there are items indicated by this.shown
-      this.focused = true;
-      // TODO Only show if no item is selected
-      if (!this.shown) this.show();
-    },
-    blur: function() {
-      this.focused = false;
-      if (!this.mousedover && this.shown) this.hide();
-    },
-    mouseenter: function(e) {
-      this.mousedover = true;
-      // TODO Re-add 'active' class to the current target 
-    },
-    mouseleave: function(e) {
-      this.mousedover = false;
-      if (!this.focused && this.shown) this.hide();
+    // Models were emptied by the preInitialization function
+    // The parent Backbone.View constructor function has finished
+    // Options have already been parsed by _context()
+    postInitialize: function() {
+      console.log('options:', this.options);
     },
   });
 
--- tests/test_typeahead.js
+++ tests/test_typeahead.js
@@ -9,265 +9,70 @@ var queryset = [
   {name: 'New York'},
 ];
 
-describe('Objects Inherited from Typeahead', function() {
-  // Create a new object that inherits from typeahead
-  var Inherited = Typeahead.extend({
-    method: function() {
-      // A blank, extended method
-    }
+describe('Typeahead', function() {
+
+  describe('Initialization', function() {
+    it('errors if no parameters are given', function() {
+      expect(function() { new Typeahead(); }).toThrow();
+    });
+
+    it('accepts an array of models as its first parameter', function() {
+      var t = new Typeahead(queryset);
+      expect(t).toBeDefined();
+    });
+
+    it('accepts an options objects as its first parameter', function() {
+      var t = new Typeahead({key: 'label'});
+      expect(t).toBeDefined();
+      expect(_.isObject(t.options)).toBe(true);
+      expect(t.options.key).toBe('label');
+    });
+
+    it('accepts an array of models and an options object', function() {
+      var t = new Typeahead(queryset, {key: 'label'});
+      expect(t).toBeDefined();
+      expect(_.isObject(t.options)).toBe(true);
+      expect(t.options.key).toBe('label');
+    });
+
+    it('sets sane defaults when not given options', function() {
+      var t = new Typeahead(queryset);
+      expect(_.isObject(t.options)).toBe(true);
+      expect(t.options.key).toBe('name');
+    });
+
   });
-  var inherit = new Inherited(queryset)
 
-  it('should function as a typeahead', function() {
-    // A queryset should have been created, but no models
-    expect(inherit.length).toEqual(0);
-    expect(inherit._queryset).toEqual(queryset);
-  });
+  // describe('Empty Search', function() {
+  //   var resetSpy;
 
-  // TODO attempt to overwrite some behaviors, such as parse, comparator
+  //   beforeEach(function() {
+  //     resetSpy = sinon.spy();
+  //   });
 
+  //   afterEach(function() {
+  //   });
+
+  // });
+
+  // End Typeahead test suite
 });
 
-describe('Improperly Configured Typeahead', function() {
+describe('Extended Typeaheads', function() {
 
-  it('can not be created', function() {
-    expect(function() { new Typeahead(); }).toThrow();
-  });
 
-});
+  it('can be created by calling extend()', function() {
+    // Create the constructor
+    var Extended = Typeahead.extend({
+      className: 'extended-class',
+    });
+    expect(Extended).toBeDefined();
 
-describe('Local Typeahead', function() {
-
-  // TODO this is only the local version
-  // TODO separate suite of test for default options?
-  var typeahead = new Typeahead(queryset);
-
-  beforeEach(function() {
-    // typeahead.search.call(typeahead, typeahead._key, '');
-  });
-
-  afterEach(function() {
+    // Create an instance
+    var t = new Extended(queryset);
+    expect(t).toBeDefined();
 
   });
 
-  it('should initialize a local collection when no URL exists', function() {
-    // Check that the local methods have been created
-    expect(typeahead.matcher).toBe(Typeahead.LocalCollection.matcher);
-    expect(typeahead.search).toBe(Typeahead.LocalCollection.search);
-
-    // A queryset should have been created, but no models
-    expect(typeahead.length).toEqual(0);
-    expect(typeahead._queryset).toEqual(queryset);
-  });
-
-  it('should have sane defaults', function() {
-    expect(typeahead._key).toBe('name');
-    // TODO minimum entry length, length of menus, and other parameters
-  })
-
-  describe('Search', function() {
-    // TODO turn off triggers after test
-    var resetSpy;
-
-    beforeEach(function() {
-      resetSpy = sinon.spy();
-      typeahead.on('reset', resetSpy);
-      typeahead.search.call(typeahead, 'name', 'ne');
-    });
-
-    afterEach(function() {
-      typeahead.off('reset', resetSpy)
-    });
-    
-    it('should filter its models', function() {
-      expect(typeahead.length).toEqual(3);
-    });
-
-    it('should fire a "reset" signal', function() {
-      expect(resetSpy.calledOnce).toBeTruthy();
-    });
-
-    it('does not overwrite the _queryset parameter', function() {
-      expect(typeahead._queryset).toEqual(queryset);
-    });
-    // End local search suite
-  });
-
-  describe('Empty Search', function() {
-    var resetSpy;
-
-    beforeEach(function() {
-      resetSpy = sinon.spy();
-      typeahead.on('reset', resetSpy);
-      typeahead.search.call(typeahead, 'name');
-    });
-
-    afterEach(function() {
-      typeahead.off('reset', resetSpy)
-    });
-
-    it('should empty the models', function() {
-      expect(typeahead.length).toEqual(0);
-    });
-
-    it('should fire a "reset" signal', function() {
-      expect(resetSpy.calledOnce).toBe(true);
-    });
-
-    it('does not overwrite the _queryset parameter', function() {
-      expect(typeahead._queryset).toEqual(queryset);
-    });
-
-    it('should leave the "selected" parameter undefined', function() {
-      expect(typeahead.selected).toBeUndefined();
-    });
-    // End empty local search suite
-  });
-
-  describe('Search Selection', function() {
-    // TODO need before and after functions
-    var selectSpy = sinon.spy();
-    typeahead.on('select', selectSpy);
-
-    it('should fire the select trigger', function() {
-      typeahead.search.call(typeahead, typeahead._key, 'mi');
-
-      // Select the first item by marking the 'li' with the 'active' class
-      typeahead.view.$menu.find('li').first().addClass('active');
-      typeahead.view.select.call(typeahead.view);
-
-      expect(selectSpy.calledOnce).toBeTruthy();
-      // Check that the first argument of the first call is a Backbone Model
-      // TODO check that the returned object is an instance of a model
-      expect(selectSpy.firstCall.args[0]).toBeDefined();
-
-      // The 'selected' boolean should be true
-      expect(typeahead.selected).toBeDefined();
-
-      // TODO test multiple calls
-      // console.log('spy:', selectSpy.callCount)
-
-      // TODO Changing the search input should remove the selected parameter
-      // TODO it should also fire a deselect event
-
-    });
-
-  });
-
-  describe('Options', function() {
-    var typeahead = new Typeahead(queryset, {key: 'label'});
-
-    it ('should allow changing the search key', function() {
-      expect(typeahead._key).toBe('label');
-    });
-  });
-
-  // End Backbone Typeahead suite
-});
-
-describe('Remote Typeahead', function() {
-
-  // TODO separate suite of test for default options?
-  // TODO Should all initialization go in the beforeEach function?
-  var typeahead = new Typeahead([], {url: '/queryset'});
-
-  // Our fake sinon.js remote API
-  var server;
-
-  // TODO build this array dynamically from the initial queryset
-  var ne_set = [
-    {name: 'Minnesota'},
-    {name: 'Nevada'},
-    {name: 'New York'},
-  ];
-  
-  it('should initialize a remote collection when an URL exists', function() {
-    // Check that the local methods have been created
-    expect(typeahead.search).toBe(Typeahead.RemoteCollection.search);
-
-    // No models should have been added
-    expect(typeahead.length).toEqual(0);
-  });
-
-  it('should have sane defaults', function() {
-    expect(typeahead._key).toBe('name');
-    // TODO minimum entry length, length of menus, and other parameters
-  })
-
-  describe('Search', function() {
-    var resetSpy;
-
-    beforeEach(function() {
-      server = sinon.fakeServer.create();
-      typeahead.search.call(typeahead, typeahead.key, '');
-
-      resetSpy = sinon.spy();
-      typeahead.on('reset', resetSpy);
-    });
-
-    afterEach(function() {
-      server.restore();
-      typeahead.off('reset', resetSpy)
-    });
-
-    it('should retrieve models from the server', function() {
-      var response = [
-        200,
-        {'Content-Type': 'application/json'},
-        '[{"name": "Minnesota"}, {"name": "Nevada"}, {"name": "New York"}]'
-      ];
-      server.respondWith("GET", "/queryset?name=ne", response);
-      typeahead.search.call(typeahead, typeahead._key, 'ne');
-      server.respond(); // Process all requests so far
-      expect(typeahead.length).toEqual(3);
-      console.log(resetSpy);
-      console.log('reset spy count', resetSpy.callCount);
-      expect(resetSpy.calledOnce).toBe(true);
-    });
-
-  });
-
-  describe('Empty Search', function() {
-    var resetSpy;
-    
-    beforeEach(function() {
-      server = sinon.fakeServer.create();
-      resetSpy = sinon.spy();
-      typeahead.on('reset', resetSpy);
-    });
-
-    afterEach(function() {
-      server.restore();
-      typeahead.off('reset', resetSpy)
-    });
-
-    it('should not make a call to the server', function() {
-
-      var response = [
-        200,
-        {'Content-Type': 'application/json'},
-        '[{"name": "Minnesota"}, {"name": "Nevada"}, {"name": "New York"}]'
-      ];
-      server.respondWith("GET", "/queryset?name=ne", response);
-
-      // Replay a working search
-      typeahead.search.call(typeahead, typeahead._key, 'ne');
-      server.respond(); // Process all requests so far
-      expect(typeahead.length).toEqual(3);
-      expect(server.requests.length).toEqual(1);
-      expect(resetSpy.calledOnce).toBe(true);
-
-      // Send an empty search
-      typeahead.search.call(typeahead, typeahead._key, '');
-      server.respond();
-      expect(server.requests.length).toEqual(1);
-      expect(typeahead.length).toEqual(0);
-      expect(resetSpy.callCount).toEqual(2);
-    });
-
-
-  });
-
-
-  // End Backbone Typeahead suite
+  // End Extended Typeahead test suite
 });
