ea8797d0fcccfaba1c989fa8662cf88d238086c3
aodin <aaronoellis@gmail.com>
2013-07-25 00:38:44 -0700

    Use a RegExp-based filter to return case insensitive matches

--- backbone.typeahead.js
+++ backbone.typeahead.js
@@ -9,7 +9,7 @@
       if (!_.isObject(options)) options = {};
     } else if (_.isObject(models)) {
       options = models;
-      // TODO Should also error if no collection was provided
+      // TODO Should also error if no collection was provided in options
     } else {
       throw new Error('A typeahead must be created with either initial models or have its collection specified in an options object');
     }
@@ -26,16 +26,34 @@
     // Pre-initialization should set up the collection type (remote / local)
     preInitialize: function(models, options) {
       // Set sane defaults
+      // TODO Allow compound keys? Introspect first model for a default key?
       options.key || (options.key = 'name');
+      options.max || (options.max = 6);
 
-      // TODO Build a collection if none was given
-      // TODO Build a item view if none was provided
+      if (_.isUndefined(options.collection) && _.isArray(models)) {
+        // TODO Any properties that passing options can't handle?
+        options.collection = new Backbone.Collection(models, options);
+      }
+
+      // TODO Build a item view if one was not provided
     },
     // Models were emptied by the preInitialization function
     // The parent Backbone.View constructor function has finished
     // Options have already been parsed by _context()
     postInitialize: function() {
-      console.log('options:', this.options);
+      // Build the cached lookup of models by the given key
+      // TODO this must be updated when the collection changes
+      // TODO provide an option to turn this cache off
+    },
+    // Return the models with a key that matches a portion of the given value
+    // TODO compound keys! cached values! the kitchen sink!
+    search: function(value) {
+      // Use a regex to quickly perform a case-insensitive match
+      var re = new RegExp(value, 'i');
+      var key = this.options.key;
+      return this.collection.filter(function(model) {
+          return re.test(model.get(key));
+      });
     },
   });
 
--- tests/test_typeahead.js
+++ tests/test_typeahead.js
@@ -1,6 +1,6 @@
 // Fixtures and name shortcut
 var Typeahead = Backbone.Typeahead;
-var queryset = [
+var states = [
   {name: 'Alabama'},
   {name: 'Michigan'},
   {name: 'Minnesota'},
@@ -17,28 +17,54 @@ describe('Typeahead', function() {
     });
 
     it('accepts an array of models as its first parameter', function() {
-      var t = new Typeahead(queryset);
+      var t = new Typeahead(states);
       expect(t).toBeDefined();
+
+      // A collection should always exist
+      expect(t.collection).toBeDefined();
+      expect(t.collection.length).toEqual(states.length);
     });
 
     it('accepts an options objects as its first parameter', function() {
-      var t = new Typeahead({key: 'label'});
+      // The options object must contain a collection or it will error
+      var items = new Backbone.Collection(states);
+      var t = new Typeahead({key: 'label', collection: items});
       expect(t).toBeDefined();
       expect(_.isObject(t.options)).toBe(true);
       expect(t.options.key).toBe('label');
     });
 
     it('accepts an array of models and an options object', function() {
-      var t = new Typeahead(queryset, {key: 'label'});
+      var t = new Typeahead(states, {key: 'label'});
       expect(t).toBeDefined();
       expect(_.isObject(t.options)).toBe(true);
       expect(t.options.key).toBe('label');
+
+      // A collection should always exist
+      expect(t.collection).toBeDefined();
+      expect(t.collection.length).toEqual(states.length);
     });
 
     it('sets sane defaults when not given options', function() {
-      var t = new Typeahead(queryset);
+      var t = new Typeahead(states);
       expect(_.isObject(t.options)).toBe(true);
       expect(t.options.key).toBe('name');
+      expect(t.options.max).toBe(6);
+
+      // A collection should always exist
+      expect(t.collection).toBeDefined();
+      expect(t.collection.length).toEqual(states.length);
+    });
+
+  });
+
+  describe('Search', function() {
+    var t = new Typeahead(states);
+
+    it('returns case-insensitive results by default', function() {
+      // TODO Does order of results matter?
+      expect(t.search('MI').length).toEqual(2);
+      expect(t.search('mi').length).toEqual(2);
     });
 
   });
@@ -61,7 +87,7 @@ describe('Typeahead', function() {
 describe('Extended Typeaheads', function() {
 
 
-  it('can be created by calling extend()', function() {
+  it('can be created by calling extend', function() {
     // Create the constructor
     var Extended = Typeahead.extend({
       className: 'extended-class',
@@ -69,7 +95,7 @@ describe('Extended Typeaheads', function() {
     expect(Extended).toBeDefined();
 
     // Create an instance
-    var t = new Extended(queryset);
+    var t = new Extended(states);
     expect(t).toBeDefined();
 
   });
