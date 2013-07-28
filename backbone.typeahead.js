21ea28826589f43f216e828356d8be6e7336a894
aodin <aaronoellis@gmail.com>
2013-07-28 16:50:56 -0700

    Returned functionality to prototype level.

--- backbone.typeahead.js
+++ backbone.typeahead.js
@@ -22,28 +22,103 @@
 
   Typeahead.extend = Backbone.View.extend;
 
+  // TODO Use a preInit/postInit style view
+  Typeahead.ItemView = Backbone.View.extend({
+    tagName: 'li',
+    events: {
+      'click': 'selectItem',
+      'mouseover': 'activateItem',
+    },
+    initialize: function(options) {
+      // A reference to the parent view (aka the Typeahead) is required
+      this.parent = options.parent;
+    },
+    render: function() {
+      this.$el.html(_.template(this.template, this.model.toJSON()));
+      return this;
+    },
+    // TODO Or use triggers instead?
+    selectItem: function() {
+      this.parent.selectModel(this.model);
+    },
+    activateItem: function() {
+      this.parent.activateModel(this.model);
+    },
+  });
+
   _.extend(Typeahead.prototype, Backbone.View.prototype, {
     // Pre-initialization should set up the collection type (remote / local)
     preInitialize: function(models, options) {
       // Set sane defaults
       // TODO Allow compound keys? Introspect first model for a default key?
       options.key || (options.key = 'name');
-      options.max || (options.max = 6);
+      options.limit || (options.limit = 6);
 
       if (_.isUndefined(options.collection) && _.isArray(models)) {
-        // TODO Any properties that passing options can't handle?
+        // TODO Any properties of collections that options can't handle?
         options.collection = new Backbone.Collection(models, options);
       }
 
-      // TODO Build a item view if one was not provided
+      // Build a item view if one was not provided
+      // TODO confirm that any given view is a Backbone.View object
+      this.view = options.view;
+
+      // TODO Uhoh, context
+      var self = this;
+      
+      if (_.isUndefined(this.view)) {
+        // TODO provide the partially extended view for users to extend
+        this.view = Typeahead.ItemView.extend({
+          // Dynamically construct a template with the key
+          // TODO Allow the item template to be passed through options
+          template: options.itemTemplate || '<a><%- ' + options.key + ' %></a>',
+        });
+      }
+
+      // Attach native events, deferring to custom events
+      this.events = _.extend({}, this.nativeEvents, _.result(this, 'events'));
+
+      // TODO Listen to changes on the collection
     },
     // Models were emptied by the preInitialization function
     // The parent Backbone.View constructor function has finished
     // Options have already been parsed by _context()
     postInitialize: function() {
-      // Build the cached lookup of models by the given key
-      // TODO this must be updated when the collection changes
-      // TODO provide an option to turn this cache off
+      // TODO Build the cached lookup of models by the given key, which must
+      // be updated when the collection changes and an option to turn off
+      this.results = []; // matched results as an array of Backbone Models
+
+      // Boolean toggles
+      this.focused = false; // Is the input in focus?
+      this.shown = false; // Is the menu shown?
+      this.mousedover = false; // Is the mouse over the typeahead (incl. menu)?
+    },
+    template: '<input type="text" placeholder="Search" /><ul class="typeahead dropdown-menu"></ul>',
+    nativeEvents: {
+      'keyup': 'keyup',
+      'keypress': 'keypress',
+      'keydown': 'keydown',
+      'blur input': 'blur',
+      'focus input': 'focus',
+      'mouseenter': 'mouseenter',
+      'mouseleave': 'mouseleave',
+    },
+    // The render function should be overwritten on extended typeaheads
+    render: function() {
+      this.$el.html(this.template);
+      this.$menu = this.$('ul');
+      this.$input = this.$('input');
+      return this;
+    },
+    // Called by searchInput and whenever models change
+    rerender: function(models) {
+      this.$menu.empty();
+      _.each(models, this.renderModel, this);
+      models.length ? this.show() : this.hide();
+    },
+    renderModel: function(model) {
+      // TODO Do not re-render views?
+      this.$menu.append((new this.view({model: model, parent: this})).render().el);
     },
     // Return the models with a key that matches a portion of the given value
     // TODO compound keys! cached values! the kitchen sink!
@@ -54,6 +129,141 @@
       return this.collection.filter(function(model) {
           return re.test(model.get(key));
       });
+    },
+    // Pull the value from the search input and re-render the matched models
+    searchInput: function() {
+      // TODO it'd be nice to limit the results during array iteration
+      this.results = this.search(this.$input.val()).slice(0, this.options.limit);
+      this.rerender(this.results);
+    },
+    select: function() {
+      // TODO This may go wrong with different templates
+      var index = this.$menu.find('.active').index();
+      this.selectModel(this.results[index]);
+    },
+    selectModel: function(model) {
+      // Update the input field with the key attribute of the select model
+      this.$input.val(model.get(this.options.key));
+      // Hide the menu
+      this.hide();
+      // TODO What other parameters should be in the trigger?
+      this.trigger('selected', model, this.collection)
+      // Empty the results
+      this.results = [];
+    },
+    activateModel: function(model) {
+      this.$menu.find('.active').removeClass('active');
+    },
+    // Misc. events
+    keyup: function(evt) {
+      switch(evt.keyCode) {
+        case 40: // Down arrow
+        case 38: // Up arrow
+        case 16: // Shift
+        case 17: // Ctrl
+        case 18: // Alt
+          break;
+        // case 9: // Tab - disabled to prevent rogue select on tabbed focus
+        // TODO tab should also leave focus
+        case 13: // Enter
+          // TODO shown needs to be returned to its original function (as an
+          // indicator of whether the menu is currently displayed or not)
+          if (!this.shown) return;
+          this.select();
+          break;
+        case 27: // escape
+          if (!this.shown) return;
+          this.hide();
+          break
+        default:
+          this.searchInput();
+      }
+      evt.stopPropagation();
+      evt.preventDefault();
+    },
+    // Menu state
+    focus: function() {
+      this.focused = true;
+      // TODO Only show the menu if no item has been selected
+      if (!this.shown) this.show();
+    },
+    blur: function() {
+      this.focused = false;
+      if (!this.mousedover && this.shown) this.hide();
+    },
+    mouseenter: function() {
+      this.mousedover = true;
+      // TODO Re-add 'active' class to the current target 
+    },
+    mouseleave: function() {
+      this.mousedover = false;
+      if (!this.focused && this.shown) this.hide();
+    },
+    // Allow the user to change their selection with the keyboard
+    keydown: function(evt) {
+      // TODO I still hate this array check
+      this.suppressKeyPressRepeat = ~$.inArray(evt.keyCode, [40,38,9,13,27]);
+      this.move(evt);
+    },
+    keypress: function(evt) {
+      // The suppressKeyPressRepeat check exists because keydown and keypress
+      // may fire for the same event
+      if (this.suppressKeyPressRepeat) return;
+      this.move(evt);
+    },
+    move: function(evt) {
+      if (!this.shown) return;
+      switch(evt.keyCode) {
+        case 9: // Tab
+        case 13: // Enter
+        case 27: // Escape
+          evt.preventDefault();
+          break;
+        case 38: // Up arrow
+          evt.preventDefault();
+          this.prevItem();
+          break;
+        case 40: // Down arrow
+          evt.preventDefault();
+          this.nextItem();
+          break;
+      }
+      evt.stopPropagation();
+    },
+    prevItem: function() {
+      // TODO should there be signals for prev and next?
+      var active = this.$menu.find('.active').removeClass('active');
+      var prev = active.prev();
+      if (!prev.length) prev = this.$menu.find('li').last();
+      prev.addClass('active');
+    },
+    nextItem: function() {
+      var active = this.$menu.find('.active').removeClass('active');
+      var next = active.next();
+      if (!next.length) next = this.$menu.find('li').first();
+      next.addClass('active');
+    },
+    // Show or hide the menu depending on the typeahead's state
+    show: function() {
+      // DO not show if there are no results
+      if (!this.results.length) return;
+      var pos = $.extend(
+        {},
+        this.$input.position(),
+        // Calling the [0] index returns the vanilla HTML object
+        {height: this.$input[0].offsetHeight}
+      );
+      this.$menu.css({
+        top: pos.top + pos.height,
+        left: pos.left
+      }).show();
+      this.shown = true;
+      return this;
+    },
+    hide: function() {
+      this.$menu.hide();
+      this.shown = false;
+      return this;
     },
   });
 
--- tests/test_typeahead.js
+++ tests/test_typeahead.js
@@ -1,17 +1,18 @@
 // Fixtures and name shortcut
 var Typeahead = Backbone.Typeahead;
 var states = [
-  {name: 'Alabama'},
   {name: 'Michigan'},
   {name: 'Minnesota'},
   {name: 'Nevada'},
   {name: 'Ohio'},
   {name: 'New York'},
+  {name: 'Alabama'},
 ];
 
 describe('Typeahead', function() {
 
   describe('Initialization', function() {
+
     it('errors if no parameters are given', function() {
       expect(function() { new Typeahead(); }).toThrow();
     });
@@ -35,10 +36,11 @@ describe('Typeahead', function() {
     });
 
     it('accepts an array of models and an options object', function() {
-      var t = new Typeahead(states, {key: 'label'});
+      var t = new Typeahead(states, {key: 'label', limit: 8});
       expect(t).toBeDefined();
       expect(_.isObject(t.options)).toBe(true);
       expect(t.options.key).toBe('label');
+      expect(t.options.limit).toBe(8);
 
       // A collection should always exist
       expect(t.collection).toBeDefined();
@@ -49,16 +51,80 @@ describe('Typeahead', function() {
       var t = new Typeahead(states);
       expect(_.isObject(t.options)).toBe(true);
       expect(t.options.key).toBe('name');
-      expect(t.options.max).toBe(6);
+      expect(t.options.limit).toBe(6);
 
       // A collection should always exist
       expect(t.collection).toBeDefined();
       expect(t.collection.length).toEqual(states.length);
     });
 
+    it('allows custom collections', function() {
+      var Alpha = Backbone.Collection.extend({
+        comparator: function(model) {
+          return model.get('name');
+        }
+      });
+      var alpha = new Alpha(states);
+
+      var t = new Typeahead({collection: alpha});
+
+      // The results should now be sorted alphabetically
+      expect(t.collection).toBe(alpha);
+    })
+
+  });
+
+  describe('Render', function() {
+
+    it('creates a rendered element', function() {
+      var t = new Typeahead(states);
+      var rendered = t.render().el;
+
+      expect(rendered).toContainHtml(Typeahead.template);
+    });
+
+    it('allows an el to passed through options', function() {
+      var span = $('<span/>');
+      var t = new Typeahead(states, {el: span});
+      t.setElement(span).render();
+
+      expect(span).toContainHtml(Typeahead.template);
+    });
+
+    it('works with setElement', function() {
+      var t = new Typeahead(states);
+      var span = $('<span/>');
+      t.setElement(span).render();
+      
+      expect(span).toContainHtml(Typeahead.template);
+    });
+
+    it('defaults to a basic item view', function() {
+      var t = (new Typeahead(states)).render();
+      t.$input.val('mi');
+      t.searchInput();
+      var firstItemView = t.$('li').first();
+      expect(firstItemView).toContainHtml('<a>' + firstItemView.text() + '</a>');
+    });
+
+    it('will render a custom item view template', function() {
+      var t = (new Typeahead(states, {itemTemplate: '<a><em><%- name %></em</a>'})).render();
+      t.$input.val('mi');
+      t.searchInput();
+      var firstItemView = t.$('li').first();
+      expect(firstItemView).toContainHtml('<a><em>' + firstItemView.text() + '</em></a>');
+    });
+
+    it('limits the number of menu items', function() {
+      var t = (new Typeahead(states, {limit: 1})).render();
+      t.$input.val('m');
+      t.searchInput();
+      expect(t.$('li')).toHaveLength(1);
+    });
   });
 
   describe('Search', function() {
+
     var t = new Typeahead(states);
 
     it('returns case-insensitive results by default', function() {
@@ -67,25 +133,101 @@ describe('Typeahead', function() {
       expect(t.search('mi').length).toEqual(2);
     });
 
+    it('populates the menu when the typeahead has been rendered', function() {
+      var span = $('<span/>');
+      t.setElement(span).render();
+      t.$input.val('mi');
+      t.searchInput();
+
+      expect(t.$('li')).toHaveLength(2);
+    });
+
   });
 
-  // describe('Empty Search', function() {
-  //   var resetSpy;
+  describe('Events', function() {
 
-  //   beforeEach(function() {
-  //     resetSpy = sinon.spy();
-  //   });
+    var t, span;
+    beforeEach(function() {
+      span = $('<span/>');
+      t = (new Typeahead(states)).setElement(span).render();
+    });
 
-  //   afterEach(function() {
-  //   });
+    it('are added natively', function() {
+      expect(t.events).toEqual(t.nativeEvents);
+    });
 
-  // });
+    it('searches the models on a character keyup', function() {
+      t.$input.val('m');
+      // Character code 109 is 'm'
+      t.$input.trigger(jQuery.Event('keyup', {keyCode: 109}));
+      expect(t.$('li')).toHaveLength(3);
+    });
 
+    it('select a model when it is clicked', function() {
+      t.$input.val('m');
+      t.searchInput();
+      expect(t.$('li')).toHaveLength(3);
+
+      var selectSpy = sinon.spy();
+      t.on('selected', selectSpy);
+      var firstMenuItem = t.$('li').first();
+      firstMenuItem.trigger('click');
+
+      expect(t.$input.val()).toEqual(firstMenuItem.text());
+      expect(selectSpy.calledOnce).toBe(true);
+
+      // The selected event should fire with a Backbone Model as an arg
+      var item = t.collection.findWhere({name: firstMenuItem.text()});
+      expect(selectSpy.firstCall.calledWith(item, t.collection));
+    });
+
+    it('move the active item with the up and down arrows', function() {
+      t.$input.val('m');
+      t.searchInput();
+
+      // Down arrow is key 40
+      t.$input.trigger(jQuery.Event('keydown', {keyCode: 40}));
+      expect(t.$('li').first()).toHaveClass('active');
+      // It should be the only item to have the active class
+      expect(t.$('.active')).toHaveLength(1);
+
+      // Up arrow is key 38
+      t.$input.trigger(jQuery.Event('keydown', {keyCode: 38}));
+      expect(t.$('li').last()).toHaveClass('active');
+      expect(t.$('.active')).toHaveLength(1);
+
+      t.$input.trigger(jQuery.Event('keydown', {keyCode: 38}));
+      // Second item should now be selected
+      expect(t.$('li').eq(1)).toHaveClass('active');
+      expect(t.$('.active')).toHaveLength(1);
+    });
+
+    it('select the active item on enter', function() {
+      t.$input.val('m');
+      t.searchInput();
+
+      // Up arrow is key 38, Enter is key 13
+      t.$input.trigger(jQuery.Event('keydown', {keyCode: 38}));
+      var activeItem = t.$('.active').first();
+      var item = t.collection.findWhere({name: activeItem.text()});
+
+      var selectSpy = sinon.spy();
+      t.on('selected', selectSpy);
+
+      t.$input.trigger(jQuery.Event('keyup', {keyCode: 13}));
+
+      expect(t.$input.val()).toEqual(activeItem.text());
+      expect(selectSpy.calledOnce).toBe(true);
+
+      // The selected event should fire with a Backbone Model as an arg
+      var item = t.collection.findWhere({name: activeItem.text()});
+      expect(selectSpy.firstCall.calledWith(item, t.collection));
+    });
+  });
   // End Typeahead test suite
 });
 
 describe('Extended Typeaheads', function() {
-
 
   it('can be created by calling extend', function() {
     // Create the constructor
@@ -97,8 +239,34 @@ describe('Extended Typeaheads', function() {
     // Create an instance
     var t = new Extended(states);
     expect(t).toBeDefined();
-
+    expect(t.render().el).toHaveClass('extended-class');
   });
+
+  it('can use a different template', function() {
+    var Ordered = Typeahead.extend({
+      template: '<input type="search" placeholder="Enter a State" /><ol class="typeahead dropdown-menu"></ol>',
+      // Render must be overwritten because of the $menu cache
+      render: function() {
+        this.$el.html(this.template);
+        this.$menu = this.$('ol');
+        this.$input = this.$('input');
+        return this;
+      },
+    });
+    expect(Ordered).toBeDefined();
+
+    var t = new Ordered(states);
+    expect(t).toBeDefined();
+
+    var rendered = t.render().el;
+    expect(rendered).toContainHtml(Ordered.template);
+
+    t.$input.val('mi');
+    t.searchInput();
+    expect(t.$('li')).toHaveLength(2);
+  });
+
+  // TODO Custom events
 
   // End Extended Typeahead test suite
 });
